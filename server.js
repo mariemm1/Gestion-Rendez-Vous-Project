f686078eb4e62e9430ff81bbc714dbc3bbfbf215
mariemjabberi94 <mariemjabberi94@gmail.com>
2025-03-09 18:15:27 +0100

    Updated server.js with new changes

--- server.js
+++ server.js
@@ -3,9 +3,16 @@
 const express = require("express"); 
 const mongoose = require('mongoose');
 require("dotenv").config();
-
+const clientRoutes = require ('./routes/client.route')
+const AuthRoutes = require ('./routes/auth.route')
+const RendezVousRoutes = require ('./routes/rendezVous.route')
 
 const app = express();
+
+app.use(express.json())
+app.use('/client',clientRoutes)
+app.use('/auth',AuthRoutes)
+app.use('/rendezVous',RendezVousRoutes)
 
 // Correction de la faute de frappe
 mongoose.connect(process.env.MONGO_URI)
