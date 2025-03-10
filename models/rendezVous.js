c9df92a969ed54cd0a046bc6719fda9968d43cd0
mariemjabberi94 <mariemjabberi94@gmail.com>
2025-03-10 14:48:47 +0100

    Updated models/rendezvous.js

--- models/rendezVous.js
+++ models/rendezVous.js
@@ -5,8 +5,7 @@ const rendezVousSchema = new mongoose.Schema({
   heure: { type: String, required: true },
   statut: { type: String, enum: ["Confirmé", "Annulé", "En attente"], default: "En attente" },
   client_id: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
-  professionnel_id: { type: mongoose.Schema.Types.ObjectId, ref: "Professionnel", required: true },
-  calendrier: [{ date: Date, heure_debut: String, heure_fin: String }], // Choosing from available slots
+  professionnel_id: { type: mongoose.Schema.Types.ObjectId, ref: "Professionnel", required: true }
 });
 
 module.exports = mongoose.model("RendezVous", rendezVousSchema);
