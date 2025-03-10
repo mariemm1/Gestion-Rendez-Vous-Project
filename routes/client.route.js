b8f5f2e14e91d7bbdaca012dadbcecfd93791f47
mariemjabberi94 <mariemjabberi94@gmail.com>
2025-03-10 14:49:39 +0100

    Updated routes/client.route.js

--- routes/client.route.js
+++ routes/client.route.js
@@ -36,68 +36,106 @@ router.get('/all', async (req, res) => {
   }
 });
 
-// READ: Get a specific client by name
-// READ: Get a specific client by user name
+// READ: Get all clients with their associated user and rendezvous history
+router.get('/allH', async (req, res) => {
+  try {
+    const clients = await Client.find()
+      .populate('userId') // Populate user details
+      .populate('historiqueRendezVous'); // Populate rendezvous history
+
+    res.status(200).send(clients);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
+
+// // READ: Get a specific client by name
+// // READ: Get a specific client by user name
+// router.get('/:nom', async (req, res) => {
+//     try {
+//       // Find the user by name
+//       const user = await User.findOne({ nom: req.params.nom });
+//       if (!user) {
+//         return res.status(404).send({ message: "User not found" });
+//       }
+  
+//       // Find the client using the user ID
+//       const client = await Client.findOne({ userId: user._id }).populate('userId');
+//       if (!client) {
+//         return res.status(404).send({ message: "Client not found" });
+//       }
+  
+//       res.status(200).send(client);
+//     } catch (error) {
+//       res.status(500).send({ message: error.message });
+//     }
+//   });
+
+  // READ: Get a specific client by name with their rendezvous history
 router.get('/:nom', async (req, res) => {
-    try {
-      // Find the user by name
-      const user = await User.findOne({ nom: req.params.nom });
-      if (!user) {
-        return res.status(404).send({ message: "User not found" });
-      }
-  
-      // Find the client using the user ID
-      const client = await Client.findOne({ userId: user._id }).populate('userId');
-      if (!client) {
-        return res.status(404).send({ message: "Client not found" });
-      }
-  
-      res.status(200).send(client);
-    } catch (error) {
-      res.status(500).send({ message: error.message });
+  try {
+    // Find the user by name
+    const user = await User.findOne({ nom: req.params.nom });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
-  });
+
+    // Find the client using the user ID and populate rendezvous history
+    const client = await Client.findOne({ userId: user._id })
+      .populate('userId')
+      .populate('historiqueRendezVous');
+
+    if (!client) {
+      return res.status(404).send({ message: "Client not found" });
+    }
+
+    res.status(200).send(client);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
   
 
-// UPDATE: Update a client by email
 router.put('/:email', async (req, res) => {
-    try {
-      // Find the user by email
-      const user = await User.findOne({ email: req.params.email });
-      if (!user) {
-        return res.status(404).send({ message: "User not found" });
-      }
-  
-      // Find the client associated with the user
-      const client = await Client.findOne({ userId: user._id }).populate('userId');
-      if (!client) {
-        return res.status(404).send({ message: "Client not found" });
-      }
-  
-      const { nom, email, pwd, historiqueRendezVous } = req.body;
-  
-      // Update client data
-      if (historiqueRendezVous) {
-        client.historiqueRendezVous = historiqueRendezVous;
-      }
-  
-      // Update user details
-      if (nom) user.nom = nom;
-      if (email) user.email = email;
-  
-      // Hash password if updated
-      if (pwd) {
-        user.pwd = await bcryptjs.hash(pwd, 10);
-      }
-  
-      await user.save();
-      await client.save();
-  
-      res.status(200).send({ message: "Client updated successfully", client });
-    } catch (error) {
-      res.status(500).send({ message: error.message });
+  try {
+    const { nom, email, pwd } = req.body;
+
+    if (!nom && !email && !pwd) {
+      return res.status(400).send({ message: "At least one field (nom, email, or pwd) is required for update." });
     }
-  });
+
+    // Find the user by email
+    const user = await User.findOne({ email: req.params.email });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    // Update user details
+    if (nom) user.nom = nom;
+    if (email) {
+      // Check if email already exists
+      const emailExists = await User.findOne({ email });
+      if (emailExists && emailExists._id.toString() !== user._id.toString()) {
+        return res.status(400).send({ message: "Email already in use" });
+      }
+      user.email = email;
+    }
+
+    // Hash password if updated
+    if (pwd) {
+      user.pwd = await bcryptjs.hash(pwd, 10);
+    }
+
+    await user.save();
+    res.status(200).send({ message: "User updated successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "An error occurred while updating the client" });
+  }
+});
+
   
 // DELETE: Delete a client by userId
 router.delete('/:id', async (req, res) => {
